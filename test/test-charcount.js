9ff69531f04d4b2aa96d3ddfceebefcbc8345d20
40234272 <gtaylor18@qub.ac.uk>
2021-12-07 16:12:33 +0000

    added testing for api

--- test/test-charcount.js
+++ test/test-charcount.js
@@ -1,9 +1,19 @@
 var expect  = require('chai').expect;
 var charcount = require('../charcount');
 
+let url = 'http://charcount.40234272.qpc.hal.davecutting.uk';
+
 it('Char Count Test', function(done) {
         var t = 'hello world';
-        var a = t.replace(/[0-9\s]/g, "");
-        expect(charcount.counter(t)).to.equal(a.length);
+        var a = 10;
+        expect(charcount.counter(t)).to.equal(a);
         done();
+});
+
+it('API Empty String Test', function(done){
+        let query = '?/text='
+        request(url + query, function(error, response, body){
+                expect(response.statusCode).to.equal(200);
+                done();
+        });
 });
