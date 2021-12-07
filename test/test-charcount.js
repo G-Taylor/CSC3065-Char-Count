caf6288e08ee4d39197b18b7e3f2c587d3c0fabb
40234272 <gtaylor18@qub.ac.uk>
2021-12-07 16:36:04 +0000

    added testing for api

--- test/test-charcount.js
+++ test/test-charcount.js
@@ -2,6 +2,7 @@ var expect  = require('chai').expect;
 var charcount = require('../charcount');
 
 let url = 'http://charcount.40234272.qpc.hal.davecutting.uk';
+let request =  new XMLHttpRequest();
 
 it('Char Count Test', function(done) {
         var t = 'hello world';
@@ -12,7 +13,7 @@ it('Char Count Test', function(done) {
 
 it('API Empty String Test', function(done){
         let query = '?/text='
-        http.request(url + query, function(error, response, body){
+        request(url + query, function(error, response, body){
                 expect(body).to.equal('{"error":true,"string":"No Text Entered","answer":0}');
                 done();
         });
